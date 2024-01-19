17b27c44df3a0d4a3690c027d47a3c35946c4944
Patrick <patrickrebecchi@hotmail.com>
2024-01-19 01:14:31 -0300

    Adicionando os @NoBlank

--- src/main/java/com/pessoal/projeto/api/produto/CadastroDeProduto.java
+++ src/main/java/com/pessoal/projeto/api/produto/CadastroDeProduto.java
@@ -1,9 +1,15 @@
 package com.pessoal.projeto.api.produto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CadastroDeProduto(
+        @NotBlank
         String nome,
+        @NotBlank
         String quantidade,
+        @NotBlank
         String preco,
+        @NotBlank
         String valorAdquirido
         ) {
 }
